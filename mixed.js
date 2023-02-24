bad8dbbb11b2fc98206313c831df3feec8034998
a-dith-ya3991 <104985561+a-dith-ya3991@users.noreply.github.com>
2023-02-24 19:15:38 +0530

    Delete mixed.js

--- mixed.js
+++
@@ -1,4 +0,0 @@
-    const mobileNumberInput = document.getElementById('mobile-number');
-    const nameInput = document.getElementById('name');
-    const statusText = document.getElementById('status');
-    document.getElementById("name-print").innerHTML = "name : " + nameInput;
